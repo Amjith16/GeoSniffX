3e2d234448a62f2757ee2fbe9dcb4b75a76c0985
Amjith16 <112405950+Amjith16@users.noreply.github.com>
2024-05-03 08:50:35 +0530

    Add files via upload

---
+++ startup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Install necessary Python modules
+echo "Installing required Python modules..."
+pip install dpkt requests PyQt5
+
+# Optional: Print a message indicating that installation is complete
+echo "Installation of required Python modules is complete."
